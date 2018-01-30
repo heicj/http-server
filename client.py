475c523cf6f97cbbc1e283a77125ab6a3ab99314
Charlie Heiner <heinercj@gmail.com>
2018-01-30 12:49:36 -0800

    set up client and server. can connect. not send recv messages correctly

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+def client(message):
+	#open a socket to server
+	
+	c = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+	#c.connect( , 5000) #takes a tuple. host name and port
+	
+	host = socket.gethostname()
+	try:
+		c.connect((host, 5000))
+	except:
+		print("connection failed")
+	else:
+		c.sendall(message.encode('utf8'))
+	
+	
+	response = c.recv(1)
+	print(response)
+	#send message passed as argument to server
+	
+	#accumulate any reply sent by server into string
+	#once recieved, close the socket
+	#return message
+	
+if __name__ == '__main__':
+	client(sys.argv[1])
---
+++ server.py
@@ -0,0 +1,35 @@
+import socket
+
+def server():
+	#start server running
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+	host = socket.gethostname()
+	address = (host, 5000)
+	s.bind(address)
+	
+	s.listen(1)
+	while True:
+		conn, addr = s.accept()
+		print(conn)
+	
+		buffer_length = 8
+		message_complete = False
+		while not message_complete:
+			
+			part = conn.recv(buffer_length)
+			response = (part.decode('utf8'))
+			if len(part)< buffer_length:
+				break
+		
+		message = response
+		s.send(message)
+		conn.sendall(message.encode('utf8'))
+	
+	
+	#continue running sending responses for any messages it recieves
+	#when user presses ctrl-d the server should cleanly exit
+	#all open sockets should be closed
+	#should accept incoming connections and echo them back exactly as received.
+	#once message sent back connection to client should close
+	
+server()
